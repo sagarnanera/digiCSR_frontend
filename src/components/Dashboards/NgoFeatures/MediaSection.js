587875bb70b3c2fabdf956a02173912325d9792b
006Darsh <21ce006@charusat.edu.in>
2023-06-08 13:57:20 +0530

    add button

--- src/components/Dashboards/NgoFeatures/MediaSection.js
+++ src/components/Dashboards/NgoFeatures/MediaSection.js
@@ -1,38 +1,39 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 import NgoNavigation from "../ngoNavigation";
-import { Box, Button, Flex, Heading, Link, Text, VStack } from '@chakra-ui/react';
+import { Box, Button, Heading, Text } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
 const MediaSection = () => {
-
-  const [blogs, setBlogs] = useState([]);
+  // const [blogs, setBlogs] = useState([]);
 
   const navigate = useNavigate();
 
   const Blogs = [
     {
       id: 1,
-      title: 'Getting Started with React',
-      author: 'John Doe',
-      date: '2023-05-30',
-      excerpt: 'Learn the basics of React and how to build interactive web applications.',
+      title: "Getting Started with React",
+      author: "John Doe",
+      date: "2023-05-30",
+      excerpt:
+        "Learn the basics of React and how to build interactive web applications.",
     },
     {
       id: 2,
-      title: 'Mastering CSS Grid Layout',
-      author: 'Jane Smith',
-      date: '2023-06-05',
-      excerpt: 'Discover the power of CSS Grid Layout and create complex responsive layouts.',
+      title: "Mastering CSS Grid Layout",
+      author: "Jane Smith",
+      date: "2023-06-05",
+      excerpt:
+        "Discover the power of CSS Grid Layout and create complex responsive layouts.",
     },
     {
       id: 3,
-      title: 'JavaScript Best Practices',
-      author: 'David Williams',
-      date: '2023-06-12',
-      excerpt: 'Improve your JavaScript skills with these coding best practices and tips.',
-    }
+      title: "JavaScript Best Practices",
+      author: "David Williams",
+      date: "2023-06-12",
+      excerpt:
+        "Improve your JavaScript skills with these coding best practices and tips.",
+    },
   ];
-
 
   // useEffect(() => {
   //   // Fetch all blogs when the component mounts
@@ -56,16 +57,21 @@ const MediaSection = () => {
           All Blogs
         </Heading>
 
-        {Blogs.length === 0 ? <Box>
-          <h2>NO Blogs</h2>
-          {/* <Button onClick={navigate("/create")} colorScheme="teal" mb={4}>Create New Blog</Button> */}
-          <Button onClick={() => navigate("/Ngo/media/create")} colorScheme="teal" mb={4}>Create New Blog</Button>
-        </Box>
-          :
-          
+        {Blogs.length === 0 ? (
+          <Box>
+            <h2>NO Blogs</h2>
+            {/* <Button onClick={navigate("/create")} colorScheme="teal" mb={4}>Create New Blog</Button> */}
+            <Button
+              onClick={() => navigate("/Ngo/media/create")}
+              colorScheme="teal"
+              mb={4}
+            >
+              Create New Blog
+            </Button>
+          </Box>
+        ) : (
           Blogs.map((blog) => (
-            
-            <Box key={blog.id} borderWidth="1px" p={4} mb={4} borderRadius="md" >
+            <Box key={blog.id} borderWidth="1px" p={4} mb={4} borderRadius="md">
               <Heading as="h2" size="lg" mb={2}>
                 {blog.title}
               </Heading>
@@ -75,8 +81,14 @@ const MediaSection = () => {
               <Text>{blog.excerpt}</Text>
             </Box>
           ))
-        }
-
+        )}
+        <Button
+          onClick={() => navigate("/Ngo/media/create")}
+          colorScheme="teal"
+          mb={4}
+        >
+          Create New Blog
+        </Button>
       </Box>
     </div>
   );
